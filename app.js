9ae3edbaee212a3ac55c9e0b5d38e91c9f400d53
Javier Gongora <jgongora@gmail.com>
2022-06-24 13:07:40 -0700

    add two more transactions to blockchain

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ let blockchain = new Blockchain(genesisBlock);
 let block = blockchain.getNextBlock([transaction]);
 blockchain.addBlock(block);
 
+let anotherTransaction1 = new Transaction("Steven", "Brian", 500);
+let anotherTransaction2 = new Transaction("Javier", "Kelsi", 700);
+let block1and2 = blockchain.getNextBlock([anotherTransaction1, anotherTransaction2]);
+blockchain.addBlock(block1and2);
+
 console.log(blockchain);
