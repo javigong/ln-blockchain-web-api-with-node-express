07de43f6c05b0fb924cbb8b87b8d1f497aef4623
Javier Gongora <jgongora@gmail.com>
2022-06-24 14:20:57 -0700

    Expose blockchain via API with Express.js

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const Block = require("./block");
 const Blockchain = require("./blockchain");
 const Transaction = require("./transaction");
 
-let transaction = new Transaction("Mary", "John", 100);
+const express = require("express");
+const app = express();
 
-let genesisBlock = new Block();
-let blockchain = new Blockchain(genesisBlock);
+app.get("/blockchain", (req, res) => {
+  let transaction = new Transaction("Mary", "John", 100);
 
-let block = blockchain.getNextBlock([transaction]);
-blockchain.addBlock(block);
+  let genesisBlock = new Block();
+  let blockchain = new Blockchain(genesisBlock);
 
-let anotherTransaction1 = new Transaction("Steven", "Brian", 500);
-let anotherTransaction2 = new Transaction("Javier", "Kelsi", 700);
-let block1and2 = blockchain.getNextBlock([anotherTransaction1, anotherTransaction2]);
-blockchain.addBlock(block1and2);
+  let block = blockchain.getNextBlock([transaction]);
+  blockchain.addBlock(block);
 
-console.log(blockchain);
+  let anotherTransaction1 = new Transaction("Steven", "Brian", 500);
+  let anotherTransaction2 = new Transaction("Javier", "Kelsi", 700);
+  let block1and2 = blockchain.getNextBlock([
+    anotherTransaction1,
+    anotherTransaction2,
+  ]);
+  blockchain.addBlock(block1and2);
+
+  res.json(blockchain)
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
+});
+
+// console.log(blockchain);
